a7dbbc26d28ee8ed84c0d9996b8762439081d562
lx <895247085>
2020-03-29 10:32:43 +0800

    提交测试

---
+++ innerdemo01/src/main/java/com/bjstq/innerdemo01/Innerdemo01Application.java
@@ -0,0 +1,13 @@
+package com.bjstq.innerdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Innerdemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Innerdemo01Application.class, args);
+    }
+
+}
---
+++ innerdemo01/src/test/java/com/bjstq/innerdemo01/Innerdemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjstq.innerdemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Innerdemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
